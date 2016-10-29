28de77c5a18b82116837770ab76f12dea1ab0ea3
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-10-29 16:15:39 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-auid=${uid:-1000}
-agid=${gid:-1000}
+auid1=${auid:-1000}
+agid1=${agid:-1000}
 
-if [[ "$auid" =~ [^a-zA-Z] ]] || [[ "$guid" =~ [^a-zA-Z] ]] || [[ "$auid" == *['!'@#\$%^\&*()_+]* ]] || [[ "$guid" == *['!'@#\$%^\&*()_+]* ]]; then
-  echo "Wrong value ID! remove this container and recreate"
-  exit
-fi
-
-if [[ "$auid" = "0" ]] || [[ "$guid" == "0" ]]; then
+if [[ "$auid1" = "0" ]] || [[ "$aguid1" == "0" ]]; then
   echo "Run in ROOT user"
 else
   echo "Run in user"
   if [ ! -d "/home/user" ]; then
-  groupadd --gid ${gid} user && \
-  useradd --uid ${uid} --gid ${gid} --create-home user
+  groupadd --gid ${agid1} user && \
+  useradd --uid ${auid1} --gid ${agid1} --create-home user
   mkdir -p /home/user/.cache/acd_cli
   ln -s /cache /home/user/.cache/acd_cli
   chown -R $uid:$gid /home/user
