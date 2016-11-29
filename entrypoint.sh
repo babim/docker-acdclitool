560d2aeefaf963db07729578547feb46954c78b6
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-29 21:46:33 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,11 +24,6 @@ export HTTP_PROXY="$PROXY"
 auid=${auid:-1000}
 agid=${agid:-1000}
 
-if id user >/dev/null 2>&1; then
-        echo "user exists"
-else
-        echo "user does not exist"
- 
 if [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
   echo "Run in ROOT user"
 else
